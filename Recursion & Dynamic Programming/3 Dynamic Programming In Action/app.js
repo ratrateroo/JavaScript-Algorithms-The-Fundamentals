bd2fbb39cf4c4e8c12782ba421621fe1e51563ea
Marcus <ratrateroo@gmail.com>
2021-02-23 23:43:14 +0800

    added dynamic programming

---
+++ Recursion & Dynamic Programming/3 Dynamic Programming In Action/app.js
@@ -0,0 +1,31 @@
+let counter = 0;
+function fib(n, memo) {
+	counter++;
+	let result;
+	if (memo[n]) {
+		return memo[n];
+	}
+	if (n === 0 || n === 1) {
+		result = 1;
+	} else {
+		result = fib(n - 1, memo) + fib(n - 2, memo);
+	}
+	memo[n] = result;
+	return result;
+}
+
+fib(5, {});
+console.log(counter);
+counter = 0;
+
+fib(6, {});
+console.log(counter);
+counter = 0;
+
+fib(20, {});
+console.log(counter);
+counter = 0;
+
+fib(30, {});
+console.log(counter);
+counter = 0;
